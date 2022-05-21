7db5937ad30de01c9d5a80b3fac1e216feb6c8d8
Pablo Yohan <pabloyohandias@gmail.com>
2022-05-21 12:45:12 -0300

    Implementação do display e logica dos botoes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,62 @@
-function criaCalc(){
+function criaCalc() {
 
-  return{
+  return {
     display: document.querySelector(".display"),
+    clear: document.querySelector(".btn-clear"),
 
+    clearDisplay(){
+      this.display.value = '';
+    },
 
+    btnParaDisplay(valor){
+      this.display.value += valor;
+    },
 
+    deleteOne(){
+      this.display.value = this.display.value.slice(0, -1); 
+    },
+    
+    fazerConta(){
+      let conta = this.display.value;
 
+      try{
+        conta = eval(conta);
+        if(!conta){
+          alert('Conta invalida');
+          return;
+        }
 
+        this.display.value = conta;
+      } catch(e){
+        alert('Conta invalida');
+      }
+    },
 
-    inicia(){
-      alert(Iniciou);
+    inicia() {
+      this.clickBotoes();
+    },
+
+    clickBotoes() {
+      document.addEventListener('click', e => {
+        const element = e.target;
+
+        if (element.classList.contains('btn-num')) {
+          this.btnParaDisplay(element.innerText);
+        }
+        if (element.classList.contains('btn-clear')) {
+          this.clearDisplay();
+        }
+        if (element.classList.contains('btn-del')) {
+          this.deleteOne();
+        }
+        if (element.classList.contains('btn-eq')) {
+          this.fazerConta();
+        }
+
+      })
     }
   };
 }
 
 const calculadora = criaCalc();
+calculadora.inicia();
